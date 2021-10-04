c411afa031c3bb4b549de64a601b91d60cf099ce
Mahmood Ahmad <mahmood073@gmail.com>
2021-10-04 07:35:57 +0200

    items structure

--- core/Sale/config/main.php
+++ core/Sale/config/main.php
@@ -26,15 +26,39 @@ return [
     */
 
     'items' => [
-        [
-            'type'         => 'T-shirt',
-            'price'        => 30.99,
-            'shipped_from' => 'US',
-            'weight'       => 0.2,
-            'unit'         => 'kg'
+        'uom' => 'kg',
+        'types' => [
+            'T-shirt' => [
+                'price'        => 30.99,
+                'shipped_from' => 'US',
+                'weight'       => 0.2,
+            ],
+            'Blouse' => [
+                'price'        => 10.99,
+                'shipped_from' => 'UK',
+                'weight'       => 0.3,
+            ],
+            'Pants' => [
+                'price'        => 64.99,
+                'shipped_from' => 'UK',
+                'weight'       => 0.9,
+            ],
+            'Sweatpants' => [
+                'price'        => 84.99,
+                'shipped_from' => 'CN',
+                'weight'       => 1.1,
+            ],
+            'Jacket' => [
+                'price'        => 199.99,
+                'shipped_from' => 'US',
+                'weight'       => 2.2,
+            ],
+            'Shoes' => [
+                'price'        => 79.99,
+                'shipped_from' => 'CN',
+                'weight'       => 1.3,
+            ]
         ]
-        
-        // ...
     ],
 
 ];
